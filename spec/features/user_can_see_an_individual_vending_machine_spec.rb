071ec38dfc47a8020c762150faa6f2b9dc0b60f4
Sierra Tucker <sierra.tucker121@gmail.com>
2021-12-27 13:19:38 -0700

    Complete User-1 snacks on machine show page

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,35 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario 'they see the name of all snacks associated with that vending machine along with their price' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack_1 = dons.snacks.create(name: 'pretzels', price: "$0.99")
+    snack_2 = dons.snacks.create(name: 'brownie bites', price: "$1.99")
+    snack_3 = dons.snacks.create(name: 'mini donuts', price: "$1.99")
+
+    visit machine_path(dons)
+
+    expect(page).to have_content(snack_1.name)
+    expect(page).to have_content(snack_1.price)
+    expect(page).to have_content(snack_2.name)
+    expect(page).to have_content(snack_2.price)
+    expect(page).to have_content(snack_3.name)
+    expect(page).to have_content(snack_3.price)
+    # save_and_open_page
+  end
+
+  scenario 'they see the average price for all the snacks in that machine' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack_1 = dons.snacks.create(name: 'pretzels', price: "$0.99")
+    snack_2 = dons.snacks.create(name: 'brownie bites', price: "$1.99")
+    snack_3 = dons.snacks.create(name: 'mini donuts', price: "$1.99")
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Average Price: #{Snack.average_price}")
+
+  end
 end
